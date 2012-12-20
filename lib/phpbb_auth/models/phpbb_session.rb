7d7c7cecd6454dc5d6b319d878c4d379fb3f18e4
Arkadiusz Buras <macbury@gmail.com>
2012-12-20 14:15:31 +0100

    Update lib/phpbb_auth/models/phpbb_session.rb

--- lib/phpbb_auth/models/phpbb_session.rb
+++ lib/phpbb_auth/models/phpbb_session.rb
@@ -1,5 +1,4 @@
 class PhpbbSession < ActiveRecord::Base
-  establish_connection "phpbb_database_#{RAILS_ENV}"
   def self.table_name() "#{PHPBB_AUTH_FORUM_DATABASE_TABLE_PREFIX}sessions" end    
     
   belongs_to :phpbb_user, :foreign_key => :session_user_id, :primary_key => :user_id
