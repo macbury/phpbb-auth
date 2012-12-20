e5aab1aabd704ba725077d5612a82c522f7de652
Arkadiusz Buras <macbury@gmail.com>
2012-12-20 14:12:43 +0100

    Update lib/phpbb_auth/phpbb_auth.rb

--- lib/phpbb_auth/phpbb_auth.rb
+++ lib/phpbb_auth/phpbb_auth.rb
@@ -1,5 +1,9 @@
-require "#{RAILS_ROOT}/config/phpbb_auth_settings"
-
+PHPBB_AUTH_FORUM_DATABASE_TABLE_PREFIX = 'phpbb_'
+PHPBB_AUTH_COOKIE_NAME = "phpbb3_7uah4"
+PHPBB_AUTH_LOCAL_USER_MODEL_NAME = "User" #this is the name of the model that you will use to store information about users in your rails app
+PHPBB_AUTH_REMOTE_REMOTE_IDENTIFIER = 'user_email' #I use email to identify somebody, you may want to use the username instead
+PHPBB_AUTH_REMOTE_LOCAL_IDENTIFIER = 'email' #this is the name of a field on your local user model that will be used to lookup the value of remote identifier.
+PHPBB_AUTH_COLUMNS_TO_DUPLICATE = {:user_website => :website} #specify any additional fields that you would like copying to your local user model 
 module PhpbbAuth  
   def current_user
     unless cookies["#{PHPBB_AUTH_COOKIE_NAME}_sid"].nil?
