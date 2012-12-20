10c745eb951dc27dedb0a8931ca8876a89ccfedc
Arkadiusz Buras <macbury@gmail.com>
2012-12-20 14:08:25 +0100

    Update lib/phpbb_auth.rb

--- lib/phpbb_auth.rb
+++ lib/phpbb_auth.rb
@@ -1,3 +1,4 @@
+RAILS_ROOT = Rails.root
 require File.dirname(__FILE__) + "/phpbb_auth/phpbb_auth"
 require File.dirname(__FILE__) + "/phpbb_auth/models/phpbb_user"
 require File.dirname(__FILE__) + "/phpbb_auth/models/phpbb_session"
